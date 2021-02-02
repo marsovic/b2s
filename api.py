0dda21571a544761d9d6fa02fe8889df8b5d7d6c
Marko <marko.arsovic@edu.ece.fr$>
2021-02-02 04:27:10 +0100

    save

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import pandas as pd
 from PFE import analyse 
 from flask import Flask, request
 
-app = Flask(__name__, static_folder='../build', static_url_path='/internal')
+app = Flask(__name__, static_folder='./build', static_url_path='/internal')
 
 def num(s):
     try:
